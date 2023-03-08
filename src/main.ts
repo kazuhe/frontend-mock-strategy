a6cffa096b000693c91ff1fb4a847826c7ef0f24
kazuhe <omc.ohara@gmail.com>
2023-03-09 00:06:57 +0900

    MSW を追加

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,9 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = await import('@/mocks/browser')
+  worker.start()
+}
+
 createApp(App).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type User = {
+  id: number
+  name: string
+}
+
+const users = ref<User[]>([])
+
+const fetchUser = async () => {
+  const res = await fetch('/users')
+  users.value = await res.json()
+}
+
+fetchUser()
+</script>
+
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/vue.svg" />
+
+    <ul>
+      <li v-for="user in users" :key="user.id">id: {{ user.id }}, name: {{ user.name }}</li>
+    </ul>
   </div>
 </template>
